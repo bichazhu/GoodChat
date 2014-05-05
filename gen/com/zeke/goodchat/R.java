e9e5e0382ea9e8c9e0dccd38477e5057d2cee1a7
bichazhu <bichazhu@gmail.com>
2014-05-05 10:40:25 -0700

    UI update and hide features for non-creators

--- gen/com/zeke/goodchat/R.java
+++ gen/com/zeke/goodchat/R.java
@@ -10,6 +10,10 @@ package com.zeke.goodchat;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int course_item_normal_bg=0x7f040001;
+        public static final int course_item_pressed_bg=0x7f040000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,57 +21,81 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int bg=0x7f020000;
+        public static final int btn_black_bg_normal=0x7f020001;
+        public static final int btn_black_bg_pressed=0x7f020002;
+        public static final int btn_blue_bg_normal=0x7f020003;
+        public static final int btn_blue_bg_pressed=0x7f020004;
+        public static final int btn_create_course_bg=0x7f020005;
+        public static final int btn_find_course_bg=0x7f020006;
+        public static final int chatting_bg=0x7f020007;
+        public static final int chatting_left_bg=0x7f020008;
+        public static final int chatting_left_bg_focused=0x7f020009;
+        public static final int chatting_left_bg_normal=0x7f02000a;
+        public static final int chatting_left_bg_pressed=0x7f02000b;
+        public static final int chatting_listview_bg=0x7f02000c;
+        public static final int chatting_right_bg=0x7f02000d;
+        public static final int chatting_right_bg_focused=0x7f02000e;
+        public static final int chatting_right_bg_normal=0x7f02000f;
+        public static final int chatting_right_bg_pressed=0x7f020010;
+        public static final int course_item_bg=0x7f020011;
+        public static final int edit_item=0x7f020012;
+        public static final int edit_item_focused=0x7f020013;
+        public static final int edit_item_normal=0x7f020014;
+        public static final int ic_launcher=0x7f020015;
     }
     public static final class id {
-        public static final int add_user=0x7f080027;
-        public static final int attendance=0x7f08002a;
-        public static final int author=0x7f080015;
-        public static final int button_Login=0x7f08001f;
-        public static final int button_SignUp=0x7f080020;
-        public static final int button_add_user=0x7f080014;
-        public static final int button_create_course=0x7f080021;
-        public static final int button_find_course=0x7f080022;
-        public static final int class_room=0x7f080025;
-        public static final int create_btn=0x7f080008;
-        public static final int create_session_code=0x7f080003;
-        public static final int create_view_btn=0x7f080004;
-        public static final int create_view_ip_textview=0x7f080000;
-        public static final int create_view_name_textview=0x7f080001;
-        public static final int create_view_nick_textview=0x7f080002;
-        public static final int edittext_Login_Password=0x7f08001e;
-        public static final int edittext_Login_Username=0x7f08001d;
-        public static final int edittext_add_user=0x7f080013;
-        public static final int edittext_new_course_name=0x7f080019;
-        public static final int find_previous=0x7f080026;
-        public static final int join_btn=0x7f080009;
-        public static final int join_session_button=0x7f080007;
-        public static final int join_session_code=0x7f080006;
-        public static final int join_session_hostIP=0x7f080005;
-        public static final int linearlayout_send_message=0x7f08000e;
-        public static final int listFooter=0x7f08001a;
-        public static final int listview_courses=0x7f080018;
-        public static final int listview_users=0x7f080012;
-        public static final int logout=0x7f080028;
-        public static final int message=0x7f080016;
-        public static final int messageInput=0x7f08001b;
-        public static final int poll=0x7f080029;
-        public static final int sendButton=0x7f08001c;
-        public static final int sendfile=0x7f08002b;
-        public static final int session_client_edittext=0x7f08000b;
-        public static final int session_client_linearlayouttmp=0x7f08000a;
-        public static final int session_client_sendbtn=0x7f08000c;
-        public static final int session_client_textview=0x7f08000d;
-        public static final int session_host_edittext=0x7f08000f;
-        public static final int session_host_sendbtn=0x7f080010;
-        public static final int session_host_textview=0x7f080011;
-        public static final int sessionroomip=0x7f080024;
-        public static final int sessionroomname=0x7f080023;
-        public static final int textview_course_name=0x7f080017;
+        public static final int add_user=0x7f090029;
+        public static final int attendance=0x7f09002d;
+        public static final int author=0x7f090016;
+        public static final int button_Login=0x7f090021;
+        public static final int button_SignUp=0x7f090022;
+        public static final int button_add_user=0x7f090015;
+        public static final int button_create_course=0x7f090023;
+        public static final int button_find_course=0x7f090024;
+        public static final int class_room=0x7f090027;
+        public static final int create_btn=0x7f090009;
+        public static final int create_session_code=0x7f090003;
+        public static final int create_view_btn=0x7f090004;
+        public static final int create_view_ip_textview=0x7f090000;
+        public static final int create_view_name_textview=0x7f090001;
+        public static final int create_view_nick_textview=0x7f090002;
+        public static final int edittext_Login_Password=0x7f090020;
+        public static final int edittext_Login_Username=0x7f09001f;
+        public static final int edittext_add_user=0x7f090014;
+        public static final int edittext_new_course_name=0x7f09001b;
+        public static final int find_previous=0x7f090028;
+        public static final int join_btn=0x7f090008;
+        public static final int join_session_button=0x7f090005;
+        public static final int join_session_code=0x7f090006;
+        public static final int join_session_hostIP=0x7f090007;
+        public static final int label=0x7f09001a;
+        public static final int linearlayout_send_message=0x7f09000f;
+        public static final int listFooter=0x7f09001c;
+        public static final int listview_courses=0x7f090019;
+        public static final int listview_users=0x7f090013;
+        public static final int logoText=0x7f09000a;
+        public static final int logout=0x7f09002b;
+        public static final int message=0x7f090017;
+        public static final int messageInput=0x7f09001e;
+        public static final int poll=0x7f09002c;
+        public static final int remove_course=0x7f09002a;
+        public static final int sendButton=0x7f09001d;
+        public static final int sendfile=0x7f09002e;
+        public static final int session_client_edittext=0x7f09000c;
+        public static final int session_client_linearlayouttmp=0x7f09000b;
+        public static final int session_client_sendbtn=0x7f09000d;
+        public static final int session_client_textview=0x7f09000e;
+        public static final int session_host_edittext=0x7f090010;
+        public static final int session_host_sendbtn=0x7f090011;
+        public static final int session_host_textview=0x7f090012;
+        public static final int sessionroomip=0x7f090026;
+        public static final int sessionroomname=0x7f090025;
+        public static final int textview_course_name=0x7f090018;
     }
     public static final class layout {
         public static final int activity_create_session_room=0x7f030000;
@@ -86,19 +114,17 @@ public final class R {
         public static final int sessionroom_rowview=0x7f03000d;
     }
     public static final class menu {
-        public static final int globalchat=0x7f070000;
-        public static final int main=0x7f070001;
-        public static final int sessionhost=0x7f070002;
+        public static final int globalchat=0x7f080000;
+        public static final int main=0x7f080001;
+        public static final int sessionhost=0x7f080002;
     }
     public static final class string {
-        public static final int AddNewUser=0x7f050004;
-        public static final int ClassRoom=0x7f050003;
-        public static final int FindPrevious=0x7f050002;
-        public static final int Logout=0x7f050001;
-        public static final int action_settings=0x7f050006;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050007;
-        public static final int title_activity_add_user=0x7f050005;
+        public static final int AddNewUser=0x7f060004;
+        public static final int ClassRoom=0x7f060003;
+        public static final int FindPrevious=0x7f060002;
+        public static final int Logout=0x7f060001;
+        public static final int RemoveCourse=0x7f060005;
+        public static final int app_name=0x7f060000;
     }
     public static final class style {
         /** 
@@ -122,10 +148,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
